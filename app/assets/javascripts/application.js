a46e4a9d83d2309ccca113b9522b65fc07e78b70
yunkmr <yuuun.k16@gmail.com>
2022-01-13 10:10:21 +0000

    [Fix]application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -83,33 +83,33 @@ $(function() {
 });
 
 
-$(document).on('turbolinks:load', function() {
+// $(document).on('turbolinks:load', function() {
 
-  const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="product[images_attributes][${index}][src]"
-                    id="product_images_attributes_${index}_src"><br>
-                    <div class="js-remove">削除</div>
-                  </div>`;
-    return html;
-  }
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+//   const buildFileField = (index)=> {
+//     const html = `<div data-index="${index}" class="js-file_group">
+//                     <input class="js-file" type="file"
+//                     name="product[images_attributes][${index}][src]"
+//                     id="product_images_attributes_${index}_src"><br>
+//                     <div class="js-remove">削除</div>
+//                   </div>`;
+//     return html;
+//   }
+//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
-  $('#image-box').on('change', '.js-file', function(e) {
+//   $('#image-box').on('change', '.js-file', function(e) {
 
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
+//     $('#image-box').append(buildFileField(fileIndex[0]));
+//     fileIndex.shift();
 
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-  });
+//     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+//   });
 
-  $('#image-box').on('click', '.js-remove', function() {
-    $(this).parent().remove();
+//   $('#image-box').on('click', '.js-remove', function() {
+//     $(this).parent().remove();
 
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  });
-});
+//     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+//   });
+// });
 
 
 // 投稿ページのタブの切り替え
